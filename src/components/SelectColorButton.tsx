593a51dfb57dfeb033483d3c287744cc31e91ead
Nuttapong9911 <nuttapongboonsala@gmail.com>
2021-09-02 16:28:11 +0700

    completed #1

--- src/components/SelectColorButton.tsx
+++ src/components/SelectColorButton.tsx
@@ -1,20 +1,29 @@
+import {PixelPainterStore, setSelectedColor} from "../stores/PixelPainterStore"
+
 type SelectColorButtonProps = {
   color: string;
 }
 
 const SelectColorButton = ({ color }: SelectColorButtonProps) => {
 
+  const state = PixelPainterStore.useState()
+
   //modify this function to highlight correctly
-  const computeRingSize = () => {
-    return ""
-    return "ring-8 ring-green-400"
+  const computeRingSize = (selectedColor:string | null) => {
+    if(selectedColor === color)
+      return "ring-8 ring-green-400"
+    else 
+      return ""
   }
 
   return (
-    <div className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
+    <button className={`${computeRingSize(state.selectedColor)} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
       style={{ backgroundColor: color }}
+      onClick={() => {
+        setSelectedColor(color)
+      }}
     >
-    </div>
+    </button>
   )
 }
 
--- src/stores/PixelPainterStore.ts
+++ src/stores/PixelPainterStore.ts
@@ -3,6 +3,7 @@ import {Store} from 'pullstate'
 type PixelPainterStoreType = {
   //we save painted color as hex code (string) in 2D array
   canvas: string[][] 
+  selectedColor: string
 }
 
 //return an (16 x 16) 2D array filled with "#FFFFFF"
@@ -18,5 +19,77 @@ const createEmptyCanvas = () => {
 }
 
 export const PixelPainterStore = new Store<PixelPainterStoreType>({
-  canvas: createEmptyCanvas()
+  canvas: createEmptyCanvas(),
+  selectedColor: "#FFFFFF"
 })
+
+export const setSelectedColor = (color:string) => {
+  PixelPainterStore.update(
+    s => {s.selectedColor = color}
+  )
+}
+
+export const setColor = (x:number,y:number) => {
+  PixelPainterStore.update(
+    s => {s.canvas[y][x] = s.selectedColor}
+  )
+}
+
+export const clear = () => {
+  PixelPainterStore.update(
+    s => {s.canvas.map(s => {
+      for(var i = 0; i< 16; i++)
+          s[i] = "#FFFFFF"
+      })
+    }
+  )
+}
+
+export const random = () => {
+  PixelPainterStore.update(
+    s => {s.canvas.map(s => {
+      for(var i = 0; i< 16; i++)
+          switch (Math.floor(Math.random() * 12) + 1) {
+            case 1:
+              s[i] = '#000000'
+              break;
+            case 2:
+              s[i] = '#804000'
+              break;
+            case 3:
+              s[i] = '#FE0000'
+              break;
+            case 4:
+              s[i] = '#FE6A00'
+              break;
+            case 5:
+              s[i] = '#FFD800'
+              break;
+            case 6:
+              s[i] = '#00FF01'
+              break;
+            case 7:
+              s[i] = '#FFFFFF'
+              break;
+            case 8:
+              s[i] = '#01FFFF'
+              break; 
+            case 9:
+              s[i] = '#0094FE'
+              break;
+            case 10:
+              s[i] = '#0026FF'
+              break;
+            case 11:
+              s[i] = '#B100FE'
+              break;
+            case 12:
+              s[i] = '#FF006E'  
+              break;   
+            default:
+              break;
+          }
+      })
+    }
+  )
+}
